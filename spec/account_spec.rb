03ac1860b974c29c0ff4346445a0d246c23b23fd
BenRoss92 <benjaminjackross@gmail.com>
2016-10-18 18:50:05 +0100

    refactored account tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,13 @@ require "account"
 
 describe Account do
 
-  subject { described_class.new }
+  subject(:account) { described_class.new }
+  let(:date) { double :date }
+
+  before(:all) do
+    @amount = 10.00
+    @larger_amount = 20.00
+  end
 
   it 'account has initial balance of 0' do
     expect(subject.balance).to eq(0)
@@ -10,23 +16,20 @@ describe Account do
 
   describe '#deposit' do
     it 'adds 10.00 to my balance' do
-      date = '18/10/16'
-      subject.deposit(10.00, date)
-      expect(subject.balance).to eq(10.00)
+      subject.deposit(@amount, date)
+      expect(subject.balance).to eq(@amount)
     end
   end
 
   describe '#withdraw' do
     it 'subtracts 10.00 to my balance' do
-      date = '19/10/16'
-      subject.deposit(20.00, date)
-      subject.withdraw(10.00, date)
-      expect(subject.balance).to eq(10.00)
+      subject.deposit(@larger_amount, date)
+      subject.withdraw(@amount, date)
+      expect(subject.balance).to eq(@amount)
     end
 
     it 'raises an error when withdrawing more than balance' do
-      date = '19/10/16'
-      expect {subject.withdraw(10.00, date)}.to raise_error "cannot withdraw more than balance"
+      expect {subject.withdraw((Account::MINIMUM_BALANCE + 1), date)}.to raise_error "cannot withdraw more than balance"
     end
   end
 
