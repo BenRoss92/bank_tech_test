ef056b09e449dcee9f29b4362e854b4ce3fbc276
BenRoss92 <benjaminjackross@gmail.com>
2016-10-18 17:44:17 +0100

    single transaction initialised as a hash

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,7 +3,7 @@ class Transaction
   attr_reader :history
 
   def initialize(amount, date, balance, transaction_type)
-    @transaction = {date => [amount, transaction_type, balance]}
+    @transaction = {date: date, amount: amount, transaction_type: transaction_type, balance: balance}
     @history = []
   end
 
